676b50dd30b68c447718ad5226ca2a98447bc2d8
Ali Ahmed Khan <aliahmed.khan01@outlook.com>
2025-03-09 22:16:48 +0100

    [chore]: update rbac permissions

--- internal/controller/deploymentrestartschedule_controller.go
+++ internal/controller/deploymentrestartschedule_controller.go
@@ -36,6 +36,7 @@ type DeploymentRestartScheduleReconciler struct {
 // +kubebuilder:rbac:groups=apps.k8s.scheduled-deployment-restart-controller.aliahmedkhan.local,resources=deploymentrestartschedules,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps.k8s.scheduled-deployment-restart-controller.aliahmedkhan.local,resources=deploymentrestartschedules/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps.k8s.scheduled-deployment-restart-controller.aliahmedkhan.local,resources=deploymentrestartschedules/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
